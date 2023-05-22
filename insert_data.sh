f438401ceac17b02022b9db93640adfdcc875c4f
Kateryna Berezovska <111088730+kberezov@users.noreply.github.com>
2023-05-22 15:08:35 -0400

    Update insert_data.sh
    Successfully inserted data from games.csv into teams and games tables.

--- insert_data.sh
+++ insert_data.sh
@@ -13,8 +13,21 @@ do
 
 if [[ $YEAR != 'year' ]]
 then
-  TEAM=$($PSQL "INSERT INTO teams(name) VALUES('$WINNER')")
-  TEAM=$($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")
+  #get winner id from teams
+  WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+  #if not found
+  if [[ -z $WINNER_ID ]]
+  then
+    WINNER_ID=$($PSQL "INSERT INTO teams(name) VALUES('$WINNER')")
+  fi
+  #get opponent id from teams
+  OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
+  #if not found
+  if [[ -z $OPPONENT_ID ]]
+  then
+    OPPONENT_ID=$($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")
 fi
-
+ #insert into games
+    INSERTED=$($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS)")
+  fi
 done
